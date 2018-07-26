129c56dbcb3d544fd91f77ab58f1638c0a818c52
Bin Shen <binshen@hioscar.com>
2018-07-25 23:46:44 -0400

    demo/

---
+++ demo/04-append-semicolon.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Root from './containers/Root'
+import configureStore from './store/configureStore'
